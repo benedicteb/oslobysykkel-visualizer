04208fd29002a475aa016db1a825ac6b681ad96f
Benedicte Emilie Brækken <b@brkn.io>
2020-05-06 14:52:23 +0200

    Add router, remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,15 @@
-import React, { useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { fetchAvailability, fetchStations } from "./services/oslobysykkel";
+import React from "react";
+import IndexPage from "./pages/IndexPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  useEffect(() => {
-    const method = async () => {
-      const stationList = await fetchAvailability();
-
-      console.log(stationList);
-    };
-
-    method();
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path={"/"}>
+        <IndexPage />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
