496086ea5ef58713eed99f4875db79d163048fb6
Benedicte Emilie Brækken <b@brkn.io>
2020-05-06 15:30:20 +0200

    Add search field

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Layout from "../components/Layout";
 
-const IndexPage = () => (
-  <>
-    <h1>Index page</h1>
-  </>
-);
+const IndexPage = () => {
+  const [searchTerms, setSearchTerms] = useState("");
+
+  return (
+    <Layout>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: "200px 0"
+        }}
+      >
+        <h1 style={{ margin: 0 }}>Søk etter bysykkelstativ</h1>
+
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <input
+            style={{
+              width: "500px",
+              borderRadius: "40px",
+              border: "0",
+              padding: "10px",
+              fontSize: "22px"
+            }}
+            type={"text"}
+            value={searchTerms}
+            onChange={event => {
+              setSearchTerms(event.target.value);
+            }}
+          />
+
+          <button
+            style={{ fontSize: "22px", margin: "0 10px", borderRadius: "5px" }}
+          >
+            Søk
+          </button>
+        </div>
+
+        <a style={{ margin: "10px 0" }} href={"/all-stations"}>
+          Se alle stasjoner
+        </a>
+      </div>
+    </Layout>
+  );
+};
 
 export default IndexPage;
